1a7e2d55838dc6a9f0e6c786471a2ffc890d82fa
ibrahim11elian <ibrahim11elian@gmail.com>
2024-03-18 15:46:00 +0200

    fix problem of getting the username from the local storage

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 // Custom hook to check if the user is authenticated
 const useAuth = () => {
   const accessToken = localStorage.getItem("token");
-  const userName = localStorage.getItem("token");
+  const userName = localStorage.getItem("user");
   // Use state to store the authentication status
   const [isAuthenticated] = useState(() => {
     // Return true if both token and user exist, otherwise return false
