199aafa2481cd9068acecf8f898e3949cf72b250
ibrahim11elian <ibrahim11elian@gmail.com>
2024-03-21 16:54:11 +0200

    add some comments

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,17 +4,26 @@ import Header from "../components/header/Header";
 import { useDispatch } from "react-redux";
 import { updateArticle } from "../features/article/article-slice";
 import useDocumentTitle from "../hooks/useDocumentTitle";
+
+// Lazy load MarkdownContent component
 const MarkdownContent = lazy(() =>
   import("../components/articleComponents/MarkdownContent")
 );
 
 function About() {
+  // Set document title
   useDocumentTitle("Script Symphony - About");
-  useEffect(() => scrollTo({ top: 0 }), []);
+
+  // Scroll to top on component mount
+  useEffect(() => {
+    scrollTo({ top: 0 });
+  }, []);
 
   const dispatch = useDispatch();
 
+  // Fetch README content from GitHub on component mount
   useEffect(() => {
+    // Fetch README content from GitHub API
     (async function fetchUserData() {
       try {
         const response = await fetch(
@@ -27,21 +36,23 @@ function About() {
         // The README content will be base64-encoded
         // Decode the base64 content as UTF-8
         const decodedContent = decodeURIComponent(escape(atob(readme.content)));
+        // Update article content in Redux store
         dispatch(updateArticle({ content: decodedContent }));
       } catch (error) {
         console.error("Error fetching README:", error);
-        return null;
       }
     })();
-  });
-  Suspense;
+  }, [dispatch]);
+
   return (
     <main>
       <Header headerText="Ibrahim Ahmed" />
       <section className="pt-4 background">
         <div className="container">
+          {/* Developer image component */}
           <DeveloperImage />
 
+          {/* Suspense for lazy-loaded MarkdownContent */}
           <Suspense fallback={<ContentPlaceholder />}>
             <MarkdownContent />
           </Suspense>
@@ -50,11 +61,14 @@ function About() {
     </main>
   );
 }
+
 export default About;
 
+// Placeholder component for Suspense fallback
 const ContentPlaceholder = () => {
   return (
     <div className="card-text placeholder-glow mt-5 d-flex flex-column gap-2 pb-1 ">
+      {/* Placeholder elements */}
       <span className="placeholder rounded-1 col-6"></span>
       <span className="placeholder rounded-1 col-7"></span>
       <h2>
